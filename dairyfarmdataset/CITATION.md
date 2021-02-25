80188c55a4ddffb8cc134fffed3e90f6f7ecee62
ChenYongyan-uu <y.chen1@uu.nl>
2021-02-25 10:29:23 +0100

    First commit

---
+++ dairyfarmdataset/CITATION.md
@@ -0,0 +1,3 @@
+Please cite this project as follows:
+
+Yongyan Chen (2021),  DairyFarmDataSet - version 0.1.0. url: github.com/ChenYongyan-uu/dairyfarmdataset
---
+++ dairyfarmdataset/docs/manuscript/Code_YChen.R
@@ -0,0 +1,80 @@
+if (!require("data.table")) {
+  install.packages("data.table", dependencies = TRUE)
+  library(data.table)
+}
+
+if (!require("dplyr")) {
+  install.packages("dplyr", dependencies = TRUE)
+  library(dplyr)
+}
+
+Data<-fread('./data/dataset_Y Chen.csv')
+glimpse(Data)
+
+# All date column change to date format
+
+if (!require("lubridate")) {
+  install.packages("lubridate", dependencies = TRUE)
+  library(lubridate)
+}
+Data$CalvingDate<-mdy(Data$CalvingDate)
+Data$TestDate<-mdy(Data$TestDate)
+Data$TestYear<-year(Data$TestDate)
+glimpse(Data)
+
+# Calculate DaysInMilk
+
+Data<-Data %>%
+  mutate(DaysInMilk=as.numeric(TestDate-CalvingDate))
+
+# Descriptive analysis
+summary(Data)
+hist(Data$MilkKG)
+hist(Data$FatPercentage)
+hist(Data$ProteinPercentage)
+hist(Data$DaysInMilk)
+n_distinct(Data$HerdId)
+n_distinct(Data$CowId)
+
+# exclude extreme value
+FilteredData<-Data %>%
+  filter(ProteinPercentage>quantile(ProteinPercentage,0.01),
+         ProteinPercentage<quantile(ProteinPercentage,0.99),
+         FatPercentage>quantile(FatPercentage,0.01),
+         FatPercentage<quantile(FatPercentage,0.99),
+         DaysInMilk<quantile(DaysInMilk,0.99)
+         )
+
+# Descriptive analysis
+summary(FilteredData)
+hist(FilteredData$MilkKG)
+hist(FilteredData$FatPercentage)
+hist(FilteredData$ProteinPercentage)
+hist(FilteredData$DaysInMilk)
+n_distinct(FilteredData$HerdId)
+n_distinct(FilteredData$CowId)
+
+# herd summary
+
+HerdSummary<-FilteredData%>%
+  group_by(HerdId,TestYear)%>%
+  summarise(HerdSize=n_distinct(CowId),
+            AverageMilkProduction=mean(MilkKG),
+            AverageProteinPercentage=mean(ProteinPercentage),
+            AverageFatPercentage=mean(FatPercentage),
+            AverageParity=mean(Parity))%>%
+  ungroup()
+
+# Plot
+if (!require("ggplot2")) {
+  install.packages("ggplot2", dependencies = TRUE)
+  library(ggplot2)
+}
+
+ggplot(HerdSummary, aes(TestYear, AverageMilkProduction)) +
+  geom_line(aes(col = HerdId, group = HerdId))+
+  geom_point(aes(col = HerdId))
+
+ggplot(HerdSummary, aes(TestYear, HerdSize)) +
+  geom_line(aes(col = HerdId, group = HerdId))+
+  geom_point(aes(col = HerdId))
